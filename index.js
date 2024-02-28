2fec80809ade2f7ac6fb257876d00aa7ff49a8d7
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2024-02-28 19:51:38 +0545

    restock action

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const redux = require('redux'); //importing redux
 
-const createsStore = redux.createStore; //creating store
+const createStore = redux.createStore; //creating store
 
-
-const effi_Wear ="effiWear"
+const effi_WearRequest ="effiWear"
+const effi_WearRestock = "Restock"
 
 //defining aciton 
-
-
 function effiWear() {
     return{
-    type: effi_Wear,
-    quantity: 1,
+    type: effi_WearRequest,
+    payload: 1,
 }
+}
+
+
+//defining action to restock
+function Restock_effiWear( qty = 1){
+    return{
+        type : effi_WearRestock,
+        payload : qty, 
+    }
 }
 
 
@@ -23,13 +30,18 @@ effiWearStock : 100,
 
 // (previousState, action) => newState
 
-
 const reducer = (state = initalState , action) =>{
     switch(action.type){
-        case effi_Wear:
+        case effi_WearRequest:
             return{
                 ...state,
                 effiWearStock: state.effiWearStock - 1,
+            }
+
+          case Restock_effiWear:
+            return{
+                ...state,
+                effiWearStock : state.effiWearStock + action.payload,
             }
             default:
                 return state ;
@@ -38,18 +50,25 @@ const reducer = (state = initalState , action) =>{
 
 
 
-const store = createsStore(reducer);
+const store = createStore(reducer);
 console.log('Initial State', store.getState());
 
 const unsubscribe = store.subscribe(()=> console.log('present state' , store.getState()));
 
+store.dispatch({
+    type: effi_WearRequest,
+    payload: 1,
+});  //dispatching action fullfilling the request
 store.dispatch(effiWear());  //dispatching action
 store.dispatch(effiWear());  //dispatching action
 store.dispatch(effiWear());  //dispatching action
 store.dispatch(effiWear());  //dispatching action
 store.dispatch(effiWear());  //dispatching action
-store.dispatch(effiWear());  //dispatching action
+store.dispatch(Restock_effiWear(10));  //dispatching action to restock 
+store.dispatch(Restock_effiWear(10));  //dispatching action to restock 
+store.dispatch(Restock_effiWear(10));  //dispatching action to restock 
 
-unsubscribe();
+unsubscribe();  //after unsubscribe it will not dispatch request
+// store.dispatch(effiWear());  //dispatching action 
 
 
