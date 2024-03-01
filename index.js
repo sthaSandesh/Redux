898fa9e6a35582429775fb1b582e4aa4aa1ec472
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2024-03-01 19:36:52 +0545

    bindaciton and

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const redux = require('redux'); //importing redux
 
 const createStore = redux.createStore; //creating store
+const bindActionCreators = redux.bindActionCreators //binding action to the store
+
 
 const effi_WearRequest ="effiWear"
 const effi_WearRestock = "Restock"
+const jacket_effiRequest = "jacketEffi"
+const jacket_effiRestock = "jacketRestock"
+
 
 //defining aciton 
 function effiWear() {
@@ -13,7 +18,6 @@ function effiWear() {
 }
 }
 
-
 //defining action to restock
 function Restock_effiWear( qty = 1){
     return{
@@ -22,10 +26,24 @@ function Restock_effiWear( qty = 1){
     }
 }
 
+function effiJacket(){
+    return{
+        type : jacket_effiRequest,
+        payload : 1 , 
+    }
+}
+
+function Restock_Jacket (qty = 1){
+    return{
+        type : jacket_effiRestock,
+        payload : qty,
+    }
+} 
 
 //defining inital state
 const initalState = {
 effiWearStock : 100,
+jacketStock : 100,
 }
 
 // (previousState, action) => newState
@@ -43,6 +61,17 @@ const reducer = (state = initalState , action) =>{
                 ...state,
                 effiWearStock : state.effiWearStock + action.payload,
             }
+        case jacket_effiRequest:
+            return{
+                ...state,
+                jacketStock : state.jacketStock - 1,
+            }
+
+          case jacket_effiRestock:
+            return{
+                ...state,
+                jacketStock : state.jacketStock + action.payload,
+            }
             default:
                 return state ;
     }
@@ -55,18 +84,31 @@ console.log('Initial State', store.getState());
 
 const unsubscribe = store.subscribe(()=> console.log('present state' , store.getState()));
 
-store.dispatch({
-    type: effi_WearRequest,
-    payload: 1,
-});  //dispatching action fullfilling the request
-store.dispatch(effiWear());  //dispatching action
-store.dispatch(effiWear());  //dispatching action
-store.dispatch(effiWear());  //dispatching action
-store.dispatch(effiWear());  //dispatching action
-store.dispatch(effiWear());  //dispatching action
-store.dispatch(Restock_effiWear(10));  //dispatching action to restock 
-store.dispatch(Restock_effiWear(10));  //dispatching action to restock 
-store.dispatch(Restock_effiWear(10));  //dispatching action to restock 
+// store.dispatch({
+//     type: effi_WearRequest,
+//     payload: 1,
+// });  //dispatching action fullfilling the request
+// store.dispatch(effiWear());  //dispatching action
+// store.dispatch(effiWear());  //dispatching action
+// store.dispatch(effiWear());  //dispatching action
+// store.dispatch(effiWear());  //dispatching action
+// store.dispatch(effiWear());  //dispatching action
+// store.dispatch(Restock_effiWear(10));  //dispatching action to restock 
+// store.dispatch(Restock_effiWear(10));  //dispatching action to restock 
+// store.dispatch(Restock_effiWear(10));  //dispatching action to restock 
+
+
+const actions = bindActionCreators({effiWear , Restock_effiWear , effiJacket , Restock_Jacket}, store.dispatch)
+actions.effiWear()
+actions.effiWear()
+actions.effiWear()
+actions.Restock_effiWear(10)
+actions.Restock_effiWear(10)
+actions.Restock_effiWear(10)
+actions.effiJacket()
+actions.effiJacket()
+actions.effiJacket()
+actions.Restock_Jacket(10)
 
 unsubscribe();  //after unsubscribe it will not dispatch request
 // store.dispatch(effiWear());  //dispatching action 
