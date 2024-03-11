fb364411db3784e93dadc84e03406fcd87c1251a
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2024-03-11 18:45:35 +0545

    state

--- react-rtk-demo/src/features/hoodies/HoodiesView.jsx
+++ react-rtk-demo/src/features/hoodies/HoodiesView.jsx
@@ -1,12 +1,22 @@
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import { ordered, restocked } from "./hoodies";
+import { useState } from "react";
 export const HoodiesView = () => {
+  const [count, setCount] = useState(1);
   const numOfHoodies = useSelector((state) => state.hoodies.numOfHoodies);
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>Number of Hoodies - {numOfHoodies}</h1>
-      <button>Order Hoodies</button>
-      <button>Restock Cake</button>
+      <button onClick={() => dispatch(ordered())}>Order Hoodies</button>
+      <input
+        type="number"
+        value={count}
+        onChange={(e) => {
+          setCount(parseInt(e.target.value));
+        }}
+      />
+      <button onClick={() => dispatch(restocked(count))}>Restock Cake</button>
     </div>
   );
 };
--- react-rtk-demo/src/features/jacket/JacketView.jsx
+++ react-rtk-demo/src/features/jacket/JacketView.jsx
@@ -1,12 +1,33 @@
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import { ordered, restocked } from "./jacket";
+import { useState } from "react";
 export const JacketView = () => {
+  const [count, setCount] = useState(1);
   const numOfJacket = useSelector((state) => state.jacket.numOfJacket);
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>Number of Jacket-{numOfJacket}</h1>
-      <button>Order Jacket</button>
-      <button>Restock Jacket</button>
+
+      <button
+        onClick={() => {
+          dispatch(ordered());
+        }}
+      >
+        Order Jacket
+      </button>
+      <input
+        type="text"
+        value={count}
+        onChange={(e) => setCount(parseInt(e.target.value))}
+      />
+      <button
+        onClick={() => {
+          dispatch(restocked(count));
+        }}
+      >
+        Restock Jacket
+      </button>
     </div>
   );
 };
